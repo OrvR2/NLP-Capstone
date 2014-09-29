fdc72df1cc4dd0be9ca592958b733d4ac168891b
Mike Zamani <michael.zamani1@gmail.com>
2014-09-29 16:54:32 -0400

    Added pickling for the tagger.

--- Unit3/tagger.py
+++ Unit3/tagger.py
@@ -3,6 +3,7 @@ from __future__ import division
 import nltk
 from nltk.corpus import brown
 from nltk.corpus import treebank
+import pickle
 
 brown_tagged_sents = brown.tagged_sents(categories='news')
 brown_sents = brown.sents(categories='news')
@@ -28,4 +29,10 @@ t0 = nltk.DefaultTagger('NN')
 t1 = nltk.UnigramTagger(train_sents, backoff=t0)
 t2 = nltk.BigramTagger(train_sents, backoff=t1)
 t3 = nltk.TrigramTagger(train_sents, backoff=t2)
+
+pickle_name = "tagger"
+pickle_object = open(pickle_name, 'wb')
+pickle.dump(t3, pickle_object)
+pickle_object.close()
+
 t3.tag(unseen_sent)
