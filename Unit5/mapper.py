25aaa4724180e9de184cce575e90e893d5a0a041
Michael Zamani <michael.zamani1@gmail.com>
2014-10-22 21:55:09 -0400

    Finalized mapper.py. Results for Named Entities for ClassEvent, Small and Large corpora.

--- Unit5/mapper.py
+++ Unit5/mapper.py
@@ -4,9 +4,6 @@ import sys, zipimport, os
 java_path = "/usr/bin/java"
 os.environ['JAVAHOME'] = java_path
 
-def is_ascii(s):
-    return all(ord(c) < 128 for c in s)
-
 importer = zipimport.zipimporter('nltk.mod')
 nltk = importer.load_module('nltk')
 nltk.data.path += ["./nltkData/"]
@@ -14,13 +11,19 @@ nltk.data.path += ["./nltkData/"]
 f = sys.stdin
 
 tagger = nltk.tag.stanford.NERTagger('./nltkData/english.all.3class.distsim.crf.ser.gz', './nltkData/stanford-ner.jar') 
-sents = nltk.tokenize.sent_tokenize(f.read().encode('utf8'))
+
+event_dir = './event'
 
 # Each line in sys.stdin will be the name of a document.
-for line in sents:
+for line in f.readlines():
     line = line.strip()
-    words = nltk.tokenize.word_tokenize(line)
-    
+    filePath = os.path.join(event_dir, line)
+    file = open(filePath, 'r')
+    words = file.read()
+    file.close()
+
+    words = nltk.tokenize.word_tokenize(words)
+
     words = [w.encode('utf8', 'ignore') for w in words if len(w.strip()) > 3 and w.isalpha()]
 
     named_entities = tagger.tag(words)
