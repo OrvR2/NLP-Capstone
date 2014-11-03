8354294c50dec6df95dc97cec80757d483488136
enderwigg <johnstark1989@gmail.com>
2014-11-03 17:10:53 -0500

    unit7 - create sentence files script

---
+++ Unit7/create_sentence_files.py
@@ -0,0 +1,31 @@
+import nltk
+from nltk.corpus import PlaintextCorpusReader
+import pickle
+import os
+from time import gmtime, strftime
+
+keywordsList = 'contained,devastating,stopped,destruction,blaze,smoldering,victims,flames,closed,deaths,residents,fire,damage,burn,heat,size,weather,smoke,houses,neighborhood,battling,acre,homes,shelter,square,wind,town,wildfire'.split(',')
+english = pickle.load(open('../nltk_data/tokenizers/punkt/english.pickle', 'r'))
+
+corpus_root = '../Texas_WildFire_Relevant'
+reader = PlaintextCorpusReader(corpus_root, '.*', sent_tokenizer=english)
+
+sents = reader.sents()
+
+folder_name = 'results_' + strftime("%Y-%m-%d %H:%M:%S", gmtime())
+os.mkdir(folder_name)
+
+# Loop through each file
+for fileid in reader.fileids():
+
+    # Get sentences from each file
+    sents = reader.sents(fileid)
+    for i, sent in enumerate(sents):
+
+        # Remove sentences under 3 words
+        if any(keyword in sent for keyword in keywordsList) and len(sent) > 3:
+            f = open(folder_name + '/' + fileid.replace('.txt','') + '-' + str(i) + '.txt', 'w')
+            f.write(' '.join(sent))
+            f.close()
+
+print 'done'
